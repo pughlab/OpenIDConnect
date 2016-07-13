d7e9fe0a788e99bf878e9ab1617784d911d63a8e
Hernando Castano <hacastan@uwaterloo.ca>
2016-07-13 12:23:17 -0400

    Replace embedded HTML with login.jade

--- examples/routes/login.js
+++ examples/routes/login.js
@@ -4,11 +4,7 @@ var oidc = require('./oidc');
 
 //Login form (I use email as user name)
 router.get('/', function(req, res, next) {
-  var head = '<head><title>Login</title></head>';
-  var inputs = '<input type="text" name="email" placeholder="Enter Email"/><input type="password" name="password" placeholder="Enter Password"/>';
-  var error = req.session.error?'<div>'+req.session.error+'</div>':'';
-  var body = '<body><h1>Login</h1><form method="POST">'+inputs+'<input type="submit"/></form>'+error;
-  res.send('<html>'+head+body+'</html>');
+  res.render('login'); 
 });
 
 var validateUser = function (req, next) {
